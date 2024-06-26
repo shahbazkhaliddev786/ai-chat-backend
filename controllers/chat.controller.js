2151996c3c5a46edb1d1ef305cd20b7a3492af3a
Shahbaz Khalid <130798658+shahbazkhaliddev786@users.noreply.github.com>
2024-06-26 11:01:18 +0500

    Update chat.controller.js

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -4,20 +4,7 @@ const axios = require("axios");
 // function to get bot response
 async function getBotResponse(prompt) {
     try {
-      const response = await axios.post(
-        'https://api.together.xyz/inference',
-        {
-          model: 'togethercomputer/llama-2-70b-chat',
-          max_tokens: 3000,
-          prompt: prompt,
-          request_type: 'language-model-inference',
-          temperature: 0.7,
-          top_p: 0.7,
-          top_k: 50,
-          repetition_penalty: 1,
-          stop: ['[/INST]', '</s>'],
-          negative_prompt: '',
-          sessionKey: '13daeea5-9e6f-4b1d-b1ba-a7e13e3b727b',
+      
         },
         {
           headers: {
